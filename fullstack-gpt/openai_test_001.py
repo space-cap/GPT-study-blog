efe4e450565cc34dabf5ac564c3b8733b3463efc
COCO <hong.git@yahoo.com>
2024-12-09 02:57:07 +0900

    Update openai_test_001.py

--- fullstack-gpt/openai_test_001.py
+++ fullstack-gpt/openai_test_001.py
@@ -2,12 +2,12 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-openai_api_key = os.getenv("OPENAI_API_KEY")
-os.environ["OPENAI_API_KEY"] = openai_api_key
+# openai_api_key = os.getenv("OPENAI_API_KEY")
+# os.environ["OPENAI_API_KEY"] = openai_api_key
 
 from langchain_openai import ChatOpenAI
 
 chat = ChatOpenAI()
-b = chat.predict("How many planets are there?")
-print(b)
+# b = chat.predict("How many planets are there?")
+# print(b)
 
