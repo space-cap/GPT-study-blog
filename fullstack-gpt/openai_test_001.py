41b3a5e4622d3e53a15ec654dd6da3b8b5e6b413
COCO <hong.git@yahoo.com>
2024-12-09 02:50:20 +0900

    Update openai_test_001.py

--- fullstack-gpt/openai_test_001.py
+++ fullstack-gpt/openai_test_001.py
@@ -1,3 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = openai_api_key
+
 from langchain.chat_models import ChatOpenAI
 
 chat = ChatOpenAI()
