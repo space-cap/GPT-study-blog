fa38fd1f76fe998b9511d9bab877874ef7475776
COCO <hong.git@yahoo.com>
2024-12-09 03:26:58 +0900

    Update gemini_test_003.py

--- fullstack-gpt/gemini_test_003.py
+++ fullstack-gpt/gemini_test_003.py
@@ -6,10 +6,11 @@ load_dotenv()
 
 # 환경 변수 가져오기
 gemini_api_key = os.getenv("Gemini_API_KEY")
+project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 
 from langchain_google_vertexai import ChatVertexAI
 
-model = ChatVertexAI(model="gemini-1.5-flash", google_api_key=gemini_api_key)
+model = ChatVertexAI(model="gemini-1.5-flash")
 
 
 
