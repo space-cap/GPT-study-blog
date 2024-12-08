ab76a896a219a084369bc50936111b05dfbceb23
COCO <hong.git@yahoo.com>
2024-12-09 03:07:56 +0900

    gemini

---
+++ fullstack-gpt/gemini_test_003.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+# .env 파일 로드
+load_dotenv()
+
+# 환경 변수 가져오기
+gemini_api_key = os.getenv("Gemini_API_KEY")
+
+from langchain_google_genai import GoogleGenerativeAI
+
+llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=gemini_api_key)
+a = llm.invoke("대한민국 수도는?")
+print(a)
+
+
+
--- fullstack-gpt/openai_test_002.py
+++ fullstack-gpt/openai_test_002.py
@@ -7,4 +7,4 @@ from langchain_openai import ChatOpenAI
 
 model = ChatOpenAI(model="gpt-4o-mini")
 
-
+model.invoke("Hello, world!")
