19ef6e3f0ae9f26f3d6b14fdb318cdf954cef303
COCO <hong.git@yahoo.com>
2024-12-09 16:50:32 +0900

    Update c_3_2_001.py

--- fullstack-gpt/c_3_2_001.py
+++ fullstack-gpt/c_3_2_001.py
@@ -11,8 +11,12 @@ chat = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 from langchain.schema import HumanMessage, AIMessage, SystemMessage
 from langchain.prompts import PromptTemplate, ChatPromptTemplate
 
-template = PromptTemplate.from_template(
-    "What is the distance between {country_a} and {country_b}.",
+template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a geography expert. And you only reply in Italian."),
+        ("ai", "Ciao, mi chiamo Paolo!"),
+        ("human", "What is the distance between Mexico and Thailand. Also, what is your name?")
+    ]
 )
 
 prompt = template.format(country_a="Seoul", country_b="Tokyo")
