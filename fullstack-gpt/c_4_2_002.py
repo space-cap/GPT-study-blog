84aaf688e7aa0ee4cc22a0f22b9a960606dccb87
COCO <hong.git@yahoo.com>
2024-12-10 20:38:46 +0900

    Update c_4_2_002.py

--- fullstack-gpt/c_4_2_002.py
+++ fullstack-gpt/c_4_2_002.py
@@ -9,7 +9,7 @@ from langchain.callbacks import StreamingStdOutCallbackHandler
 from langchain.prompts import ChatPromptTemplate
 
 chat = ChatGoogleGenerativeAI(model="gemini-1.5-flash", 
-                              temperature=0.1,
+                              temperature=0,
                               disable_streaming=False,
                               callbacks=[
                                   StreamingStdOutCallbackHandler(),
@@ -48,5 +48,29 @@ examples = [
     },
 ]
 
+example_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("human", "What do you know about {country}?"),
+        ("ai", "{answer}"),
+    ]
+)
 
+prompt = FewShotChatMessagePromptTemplate(
+    example_prompt=example_prompt,
+    examples=examples,
+)
 
+# print(prompt)
+
+final_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a geography expert, you give short answers."),
+        prompt,
+        ("human", "What do you know about {country}?"),
+    ]
+)
+
+chain = final_prompt | chat
+
+result = chain.invoke({"country": "Thailand"})
+print(result.content)
