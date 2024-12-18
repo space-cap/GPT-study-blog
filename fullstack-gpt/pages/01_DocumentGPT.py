53f737647915233f2ef578bc29a9dea0cb1d1fa8
COCO <hong.git@yahoo.com>
2024-12-18 21:23:44 +0900

    7.5

--- fullstack-gpt/pages/01_DocumentGPT.py
+++ fullstack-gpt/pages/01_DocumentGPT.py
@@ -8,34 +8,5 @@ st.set_page_config(
 
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
 
 
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
-
-
-for message in st.session_state["messages"]:
-    send_message(
-        message["message"],
-        message["role"],
-        save=False,
-    )
-
-
-message = st.chat_input("Send a message to the ai ")
-
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
-
-    with st.sidebar:
-        st.write(st.session_state)
-
-
-        
