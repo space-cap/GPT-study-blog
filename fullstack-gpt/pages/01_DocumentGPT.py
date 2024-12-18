f1d6ed692573d1321a4b44f130f907bfee35b514
COCO <hong.git@yahoo.com>
2024-12-19 00:10:57 +0900

    Update 01_DocumentGPT.py

--- fullstack-gpt/pages/01_DocumentGPT.py
+++ fullstack-gpt/pages/01_DocumentGPT.py
@@ -1,4 +1,9 @@
 import time
+from langchain.document_loaders import TextLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
 import streamlit as st
 
 st.set_page_config(
@@ -8,5 +13,35 @@ st.set_page_config(
 
 st.title("DocumentGPT")
 
+st.markdown(
+    """
+    Welcome!
+                
+    Use this chatbot to ask questions to an AI about your files!
+    """
+)
 
+file = st.file_uploader(
+    "Upload a .txt .pdf or .docx file",
+    type=["pdf", "txt", "docx"],
+)
 
+if file:
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = TextLoader("./files/chapter_one.txt")
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    s = retriever.invoke("winston")
+    s
