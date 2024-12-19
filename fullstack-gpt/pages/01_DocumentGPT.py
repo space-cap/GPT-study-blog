8aff163724871c48d6d416d9d9b1a4e8e6cd4fd7
COCO <hong.git@yahoo.com>
2024-12-19 23:10:33 +0900

    Update 01_DocumentGPT.py

--- fullstack-gpt/pages/01_DocumentGPT.py
+++ fullstack-gpt/pages/01_DocumentGPT.py
@@ -25,9 +25,9 @@ def embed_file(file):
         chunk_size=600,
         chunk_overlap=100,
     )
-    loader = UnstructuredFileLoader(file_path)
+    loader = TextLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings()
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
@@ -79,4 +79,3 @@ else:
     st.session_state["messages"] = []
 
 
-    
