644e63877851869a6d09914cdea24c2e191fdff0
COCO <hong.git@yahoo.com>
2024-12-22 16:28:13 +0900

    03

--- fullstack-gpt/pages/03_QuizGPT.py
+++ fullstack-gpt/pages/03_QuizGPT.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from langchain.retrievers import WikipediaRetriever
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
+
 
 st.set_page_config(
     page_title="QuizGPT",
@@ -6,6 +10,26 @@ st.set_page_config(
 )
 
 st.title("QuizGPT")
+
+
+@st.cache_resource(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+
+
 
 with st.sidebar:
     choice = st.selectbox(
@@ -21,6 +45,14 @@ with st.sidebar:
             "Upload a .docx , .txt or .pdf file",
             type=["pdf", "txt", "docx"],
         )
+        if file:
+            docs = split_file(file)
+            st.write(docs)
        
     else:
         topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=1)
+            with st.status("Searching Wikipedia..."):
+                docs = retriever.get_relevant_documents(topic)
+                st.write(docs)
