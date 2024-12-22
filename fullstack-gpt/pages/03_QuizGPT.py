0f5a1ecc8a214a63236c7099f648d4574a5e0180
COCO <hong.git@yahoo.com>
2024-12-22 16:12:42 +0900

    Update 03_QuizGPT.py

--- fullstack-gpt/pages/03_QuizGPT.py
+++ fullstack-gpt/pages/03_QuizGPT.py
@@ -6,3 +6,21 @@ st.set_page_config(
 )
 
 st.title("QuizGPT")
+
+with st.sidebar:
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx , .txt or .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
+       
+    else:
+        topic = st.text_input("Search Wikipedia...")
