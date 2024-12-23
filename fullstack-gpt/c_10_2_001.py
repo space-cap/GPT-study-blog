c2eac7c8b7b52d17f129d269b0f4835a88497b0a
space-cap <hong.git@yahoo.com>
2024-12-23 19:43:29 +0900

    10.2

---
+++ fullstack-gpt/c_10_2_001.py
@@ -0,0 +1,43 @@
+from langchain.document_loaders import SitemapLoader
+import streamlit as st
+
+
+@st.cache_resource(show_spinner="Loading website...")
+def load_website(url):
+    loader = SitemapLoader(url, verify_ssl=False)
+    loader.requests_per_second = 5
+    docs = loader.load()
+    return docs
+
+
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
+
+st.markdown(
+    """
+    # SiteGPT
+            
+    Ask questions about the content of a website.
+            
+    Start by writing the URL of the website on the sidebar.
+"""
+)
+
+
+with st.sidebar:
+    url = st.text_input(
+        "Write down a URL",
+        placeholder="https://example.com",
+    )
+
+
+if url:
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("Please write down a Sitemap URL.")
+    else:
+        docs = load_website(url)
+        st.write(docs)
