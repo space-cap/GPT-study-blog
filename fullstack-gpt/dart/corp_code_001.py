ba66a1690ffbda7cef154091e25761cc511b5ac9
COCO <hong.git@yahoo.com>
2024-12-26 10:50:53 +0900

    Update corp_code_001.py

--- fullstack-gpt/dart/corp_code_001.py
+++ fullstack-gpt/dart/corp_code_001.py
@@ -5,10 +5,18 @@
 
 
 import dart_fss
+import os
+from dotenv import load_dotenv
+
+
+# .env 파일 로드
+load_dotenv()
+
+# API 키 가져오기
+dart_api_key = os.getenv('DART_API_KEY')
 
 # API 키 설정
-api_key = "YOUR_API_KEY"
-dart_fss.set_api_key(api_key)
+dart_fss.set_api_key(dart_api_key)
 
 # 기업 목록 가져오기
 corp_list = dart_fss.get_corp_list()
@@ -18,3 +26,5 @@ samsung = corp_list.find_by_name('삼성전자', exactly=True)[0]
 
 # 고유번호 출력
 print(f"삼성전자의 고유번호: {samsung.corp_code}")
+
+
